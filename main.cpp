33b6ff053fed0b5e4231aaae8bdb462634afd6e1
tki2mi <ws130575329@gmail.com>
2023-10-27 18:11:49 +0800

    22

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile styleFile(":/styles/stylle.qss");
+    styleFile.open(QFile::ReadOnly | QFile::Text);
+    QTextStream styleStream(&styleFile);
+    qApp->setStyleSheet(styleStream.readAll());
     MainWindow w;
     w.show();
     return a.exec();
